ee763efa3193d8f91b7e095907a72985e96f44a7
silvaton <tdasilva@ext.laligatech.com>
2023-07-06 19:02:34 +0200

    created the project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Ton Silva on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
